025b9ce981000b5f96842ab192395e3210e321db
ecjuncal <evanjuncal@gatech.edu>
2018-01-28 17:45:23 -0500

    Removed Main Method
    Removed main method after debugging code.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -3,60 +3,55 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 4
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
 public class Person4 {
   /** Holds the persons real name */
   private String name;
-    /**
-     * The constructor, takes in the persons
-     * name
-     * @param pname the person's real name
-     */
+  /**
+   * The constructor, takes in the persons
+   * name
+   * @param pname the person's real name
+   */
   public Person4(String pname) {
     name = pname;
   }
-    /**
-     * This method should return a string
-     * where each character is 1 greater 
-     * than its previous value.  So
-     * given "abc123" it should return
-     * "bcd234".
-     *
-     * @param input the string to be modified
-     * @return the modified string
-     */
-    private String calc(String input) {
-      String transformed = "";
-      char temp;
+  /**
+   * This method should return a string
+   * where each character is 1 greater
+   * than its previous value.  So
+   * given "abc123" it should return
+   * "bcd234".
+   *
+   * @param input the string to be modified
+   * @return the modified string
+   */
+  private String calc(String input) {
+    String transformed = "";
+    char temp;
 
-      for (int i = 0; i < input.length(); i++) {
-        temp = input.charAt(i);
-        temp += 1;
+    for (int i = 0; i < input.length(); i++) {
+      temp = input.charAt(i);
+      temp += 1;
 
-        transformed += temp.toString();
-      }
-
-      return null;
-    }
-    
-    /**
-     * Return a string rep of this object
-     * that varies with an input string
-     *
-     * @param input the varying string
-     * @return the string representing the 
-     *         object
-     */
-    public String toString(String input) {
-      return name + calc(input);
+      transformed += String.valueOf(temp);
     }
 
-    public static void main(String[] args) {
-        Person4 Evan = new Person4("Evan");
-        Evan.toString("abc123")
-    }
+    return transformed;
+  }
+
+  /**
+   * Return a string rep of this object
+   * that varies with an input string
+   *
+   * @param input the varying string
+   * @return the string representing the
+   *         object
+   */
+  public String toString(String input) {
+    return name + calc(input);
+  }
 }
